9bbad2c295b4f84d92e694bdfafbee27d68fd1eb
adizdar <adizdar1@hotmail.com>
2015-09-14 22:53:41 +0200

    New calendar, resolution adaptation

--- app/scripts/controllers/calendar.controller.js
+++ app/scripts/controllers/calendar.controller.js
@@ -1,88 +1,153 @@
-(function () {
+(function() {
 
-    'use strict';
+  'use strict';
 
-    angular
-        .module('neofen.controllers')
-        .controller('CalendarController', CalendarController);
+  angular
+    .module('neofen.controllers')
+    .controller('CalendarController', CalendarController);
 
-    CalendarController.$inject = ['$scope', '$log', '$ionicPopup', '$cordovaCalendar'];
+  CalendarController.$inject = ['$scope', '$log', '$ionicPopup', '$cordovaCalendar', '$window', '$timeout'];
 
-    function CalendarController($scope, $log, $ionicPopup, $cordovaCalendar) {
+  function CalendarController($scope, $log, $ionicPopup, $cordovaCalendar, $window, $timeout) {
 
-        var vm = this;
-        var helpDateText = "U sekcijama OD i DO unosite broj dana trajanja podsjetnika u kalendaru ( Npr. od 25.6 do 29.6 će vam stizati notifikacije o podsjetniku). Interval također može biti isti dan.";
-        var helpTimeText = "U sekcijama OD i DO unosite vremenski interval trajanja podsjetnika u kalendaru. U tom intervalu će interni kalendar mobilnog uređaja postaviti podsjetnik aktivnim. (Npr. Od 18:00h do 20:00h )";
-        var helpPillsText = "U početak unosite vrijeme prve tablete, u razmak sekciji unosite koliko svakih sati morate piti tabletu."
-        
-        vm.dateFrom = null;
-        vm.dateTo = null;
-        vm.timeTo = null;
-        vm.timeFrom = null;
-        vm.calendarEventTitle = null;
-        
-        vm.submitDate = submitDate;
-        vm.help = help;
-       
-        // @todo create a one time reminder for repeat days
-        function submitDate() {
-            var startDate, endDate;
-            
-            if(!vm.calendarEventTitle) {
-                $ionicPopup.alert({
-	                title: 'Kategorija',
-	                template: 'Molimo vas označite kategoriju iz menija.'
-	            });
-                return;
-             }
-            
-            if(!vm.dateFrom || !vm.timeFrom || !vm.dateTo || !vm.timeTo) {
-                $ionicPopup.alert({
-	                title: 'Datum i vrijeme',
-	                template: 'Molimo vas unesite datum i vrijeme.'
-	            });
-                return;
-             }
-            
-            startDate = new Date(vm.dateFrom.getFullYear(),
-                                     vm.dateFrom.getMonth(),
-                                     vm.dateFrom.getDate(),
-                                     vm.timeFrom.getHours(),
-                                     vm.timeFrom.getMinutes(), 0, 0, 0);
-            
-            endDate = new Date(vm.dateTo.getFullYear(),
-                                     vm.dateTo.getMonth(),
-                                     vm.dateTo.getDate(),
-                                     vm.timeTo.getHours(),
-                                     vm.timeTo.getMinutes(), 0, 0, 0);
-            
-            $cordovaCalendar.createEvent({
-                title: vm.calendarEventTitle,
-                location: '',
-                notes: 'Podsjetnik kreiran iz Neofen aplikacije!',
-                startDate: startDate,
-                endDate: endDate
-              }).then(function (result) {
-                $ionicPopup.alert({
-	                title: 'Podsjetnik',
-	                template: 'Podsjetnik uspješno kreiran i unesen u kalendar mobilnog uređaja,' + 
-                    ' više informacija o kreiranom podsjetniku kao i izmjene podsjetnika, možete izvršiti u kalendaru mobilnog uređaja!'
-	            });
-              }, function (err) {
-                 $ionicPopup.alert({
-	                title: 'Podsjetnik',
-	                template: 'Došlo je do greške molimo vas pokusajte ponovo!'
-	            });
-            });
-        }
-        
-        function help(param) {
-            $ionicPopup.alert({
-	                title: param === 'date' ? 'Datum' : 'Vrijeme',
-	                template: param === 'date' ? helpDateText : helpTimeText
-	        });
-        }
-        
+    var vm = this;
+
+    vm.repeat = vm.repeatInterval = null;
+    vm.showHeader = false;
+    vm.isMedicineCategory = false;
+    vm.calendarData = {
+      date: null,
+      dateTo: null,
+      time: null,
+      repeat: '',
+      calendarEventTitle: null,
+      alarm: null,
+      title: null
+    };
+    vm.options = {
+      disable: [{
+        from: [1990, 1, 1],
+        to: new Date(new Date - 86700000)
+      }]
+    };
+
+    vm.formatDateForCalendarApi = formatDateForCalendarApi;
+    vm.chooseCategory = chooseCategory;
+    vm.submitDate = submitDate;
+    vm.setDate = setDate;
+
+    // Calendar methods
+    function submitDate() {
+      var date, dateTo, hours,
+          daysBetweenDate, calendarOptions;
+
+      if (!vm.calendarData.calendarEventTitle) {
+        $ionicPopup.alert({
+          title: 'Kategorija',
+          template: 'Molimo vas označite kategoriju iz menija.'
+        });
+        return;
+      }
+
+      if (window.plugins && window.plugins.calendar) {
+        calendarOptions = window.plugins.calendar.getCalendarOptions();
+        calendarOptions.firstReminderMinutes = +vm.calendarData.alarm;
+      }
+
+      if (vm.isMedicineCategory) {
+        vm.repeatInterval = +vm.calendarData.repeat - 1; // because we already save the first entry
+        vm.calculateIntervalForRepeat = calculateIntervalForRepeat(); // every time we want a new temp value
+
+        calendarOptions.recurrence = 'daily';
+        calendarOptions.recurrenceEndDate = vm.formatDateForCalendarApi(vm.calendarData.dateTo);
+        calendarOptions.recurrenceInterval = 1;
+      }
+
+      date = new Date(vm.calendarData.date.getFullYear(),
+        vm.calendarData.date.getMonth(),
+        vm.calendarData.date.getDate(),
+        vm.calendarData.time.getHours(),
+        vm.calendarData.time.getMinutes(), 0, 0, 0);
+
+      setDate(date, calendarOptions);
     }
+
+    function callbackError(err) {
+      $log.log(err);
+      $ionicPopup.alert({
+        title: 'Podsjetnik',
+        template: 'Došlo je do greške molimo vas pokusajte ponovo!'
+      });
+    }
+
+    function chooseCategory(title) {
+      vm.calendarData.calendarEventTitle = title;
+      vm.showHeader = true;
+      vm.isMedicineCategory = (title === 'Lijekovi');
+    }
+
+    function callbackSuccess(result) {
+      if (this.isMedicineCategory && this.repeatInterval) {
+        this.setDate(this.formatDateForCalendarApi(this.calendarData.date, this.calendarData.time, this.calculateIntervalForRepeat()), {
+          recurrence: 'daily',
+          recurrenceInterval: 1,
+          recurrenceEndDate: this.formatDateForCalendarApi(this.calendarData.dateTo)
+        });
+        this.repeatInterval--;
+      } else {
+        $ionicPopup.alert({
+          title: 'Podsjetnik',
+          template: 'Podsjetnik je generisan i spašen u kalendar, sve izmjene nad njim možete učiniti iz kalendara mobilnog uređaja.'
+        });
+      }
+    }
+
+    function setDate(date, calendarOptions) {
+      var options = {
+        title: vm.calendarData.calendarEventTitle + ' ' + vm.calendarData.title,
+        location: '',
+        notes: 'Podsjetnik kreiran iz Neofen aplikacije!',
+        startDate: date, // end date needs to be in this format so we don't get a all day event
+        endDate: new Date(date.getFullYear(),
+          date.getMonth(),
+          date.getDate(),
+          date.getHours() + 1,
+          date.getMinutes(), 0, 0, 0),
+        firstReminderMinutes: +vm.calendarData.alarm,
+      };
+
+      if (calendarOptions instanceof Object) {
+        for (var key in calendarOptions) {
+          if (calendarOptions.hasOwnProperty(key))
+            options[key] = calendarOptions[key];
+        }
+      }
+
+      $cordovaCalendar.createEventWithOptions(options)
+                      .then(callbackSuccess.bind(vm), callbackError);
+    }
+
+    // Helper methods
+    function formatDateForCalendarApi(date, time, hourDelay) {
+      if (!hourDelay) hourDelay = 0;
+      return new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        date.getDate(), (time ? time.getHours() + hourDelay : date.getHours() + hourDelay), (time ? time.getMinutes() : date.getMinutes()), 0, 0, 0);
+    }
+
+    function calculateIntervalForRepeat() {
+      var temp = 0;
+
+      return function() {
+        var hours = 24 / +vm.calendarData.repeat
+        return temp = temp + hours; // calcualte only for one day
+      }
+    }
+
+    function daysBetween(timeOne, timeTwo) {
+      return Math.round(Math.abs(timeOne - timeTwo) / 8.64e7);
+    }
+  }
 
 })();
